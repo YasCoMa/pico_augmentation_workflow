32c69e7662674d83a46e356d3c6b955320f2c12d
ymartins <ycfrenchgirl2@gmail.com>
2025-09-03 19:57:16 +0200

    update

--- work/57/fab76c28f70dd8a510a52e22c63c2f/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /aloy/home/ymartins/match_clinical_trial/ctpico_validation_workflow/modules/validation.py -execDir validation_trials -paramFile validation_config.json
--- work/bc/6ba91c127d96e6c8a9bb3f060da821/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-if [ ! -d "/aloy/home/ymartins/match_clinical_trial/validation_trials/" ]; then
-    mkdir /aloy/home/ymartins/match_clinical_trial/validation_trials/
-fi
